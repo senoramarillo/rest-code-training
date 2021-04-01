dd3c55982db203aff90587b88e818ecd98a8786d
U737700 <andy.wong@lhind.dlh.de>
2021-04-01 18:45:45 +0200

    #added Student example for Sprint Boot

--- src/main/java/com/rest/restcodetraining/RestCodeTrainingApplication.java
+++ src/main/java/com/rest/restcodetraining/RestCodeTrainingApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@RestController
 public class RestCodeTrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestCodeTrainingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestCodeTrainingApplication.class, args);
+    }
+
+//    @GetMapping
+//    public List<Student> hello() {
+//        return List.of(new Student(
+//        		1L,
+//                "Tom",
+//                "tom.james@gmail.com",
+//                LocalDate.of(2000, Month.JANUARY, 1),
+//                21));
+//    }
 
 }
---
+++ src/main/java/com/rest/restcodetraining/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.rest.restcodetraining.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student tom = new Student(
+                    1L,
+                    "Tom",
+                    "tom.james@gmail.com",
+                    LocalDate.of(2000, JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, JANUARY, 5)
+            );
+
+            repository.saveAll(List.of(tom, alex));
+        };
+    }
+}
